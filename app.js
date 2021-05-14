5b0215d12e2927e41eb37a18bb3bc6d572b13259
Samuel oriaku <oriaku.samuel@gmail.com>
2021-05-14 16:48:32 +0000

    register order route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 const menuRouter = require('./routes/menus');
+const orderRouter = require('./routes/orders');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', usersRouter);
 
 app.use(menuRouter);
+app.use(orderRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
